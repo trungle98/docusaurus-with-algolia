8860f27dc331f526c3365013a6d749518690ede3
trung <trung@gmadas.com>
2022-09-18 08:10:22 -0400

    ihkihl
    hli

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/trung/Desktop/NAB-research/fix-the-book/currNodeversion/my-website/node_modules/infima/dist/css/default/default.css'),
-  require('/home/trung/Desktop/NAB-research/fix-the-book/currNodeversion/my-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/trung/Desktop/NAB-research/fix-the-book/currNodeversion/my-website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/trung/Desktop/NAB-research/fix-the-book/currNodeversion/my-website/src/css/custom.css'),
-];
